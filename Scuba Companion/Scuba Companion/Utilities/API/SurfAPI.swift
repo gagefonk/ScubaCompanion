453ce7eff14ee73e8c0a98702046e8bfedbdaccb
Gage Fonk <gagefonk@Gages-MacBook-Pro.local>
2021-11-24 17:42:49 -0800

    removed google plist and surf api plist references

--- Scuba Companion/Scuba Companion/Utilities/API/SurfAPI.swift
+++ Scuba Companion/Scuba Companion/Utilities/API/SurfAPI.swift
@@ -10,19 +10,4 @@ import Foundation
 
 struct SurfAPI {
     
-    let token: String = {
-        var plistDictionary: NSDictionary?
-        if let path = Bundle.main.path(forResource: "api", ofType: "plist") {
-            plistDictionary = NSDictionary(contentsOfFile: path)
-        }
-        return plistDictionary?["token"] as! String
-    }()
-    
-    let baseURL: String = {
-        var plistDictionary: NSDictionary?
-        if let path = Bundle.main.path(forResource: "api", ofType: "plist") {
-            plistDictionary = NSDictionary(contentsOfFile: path)
-        }
-        return plistDictionary?["baseApiURL"] as! String
-    }()
 }
